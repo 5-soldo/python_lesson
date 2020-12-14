50a2a3a1b8c4d82b75edc9b3df9647621b0a6525
5-soldo <g.vavi17@gmail.com>
2020-12-14 18:15:25 +0300

    [upd] дз1_2

--- lesson_1/homework_1_2.py
+++ lesson_1/homework_1_2.py
@@ -1,3 +1,5 @@
 time = int(input("time in seconds: "))
-hours = time / 3600
-print(hours)
+hours = time // 3600
+minutes = (time // 60) - (hours * 60)
+sec = time % 60
+print(f'{hours:00}: {minutes:00}: {sec:00}')
