e0d8162832fea35e8330a6e91e94eba8eb4eb55f
5-soldo <g.vavi17@gmail.com>
2020-12-24 19:18:54 +0300

    homework_3_2.py

---
+++ lesson_3/homework_3_2.py
@@ -0,0 +1,10 @@
+
+
+def my_func(name, surname, year, phone):
+    result = f'{name}, {surname}, {year}, {phone}'
+    return result
+
+result = my_func(name='max', surname='ivanov', year=1986, phone=89038894656)
+print(result)
+print(my_func('rhbc', 'fhf', 81841, 88888888))
+
